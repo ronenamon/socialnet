8b37297771491d9d52a1f90e9744cab966029cb4
Ronen Amon <ronenamon88@gmail.com>
2021-04-20 23:20:30 +0300

    auth with ui Vuejs

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user', 'User@index');
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
